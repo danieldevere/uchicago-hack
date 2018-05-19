2cc9ceaf452336a0af8d422955ce56a39e778cf9
Dan DeVere <danieldevere285@gmail.com>
2018-05-19 11:10:15 -0500

    added part of patient model

--- patient-tracker/src/app/model/patient.ts
+++ patient-tracker/src/app/model/patient.ts
@@ -1,8 +1,64 @@
-export module ProjectAPI  {
-    export interface Patient {
-        id?: string;
-        firstname: string;
-        lastname: string;
-        age: number;
+export interface Patient {
+    id?: string;
+    witness: {
+        firstname?: string;
+        lastname?: string;
+        age?: number;
+        sex?: string;
+        location?: string;
+        description?: string;
+        time?: Date;
+        mechanismOfInjury?: string[];
+        bodyPartsAffected?: string;
+        symptomDescription?: string;
+        sensationMovesOrRadiates?: string;
+        howLongSymptomsLasted?: string;
+        treatmentSoFar?: string;
+        photoUrls?: string[];
+        areEyeOpen?: boolean;
+        reactToVoice?: boolean;
+        reactToPain?: boolean;
+        bleeding?: boolean;
+        havePulse?: boolean;
+        language?: string;
+        preExistingIllnesses?: string;
+        medications?: string;
+        allergies?: {
+            food?: string;
+            antibiotics?: string;
+            environmental?: string;
+        };
+        hospitalizedRecently?: boolean;
+        hadSurgery?: boolean;
+    };
+    ems: {
+        tech1Name?: string;
+        tech2Name?: string;
+        shiftDate?: Date;
+        emergencyType: string;
     }
+    
+        
 }
+
+export const 
+
+export const MechanismOfInjury = [
+    "Pregnancy",
+    "Gunshot",
+    "Penetrations of Head/Chest/Abdomen",
+    "Fractures",
+    "Hanging",
+    "Drowning/Suffocation",
+    "Amputation",
+    "Ejection from Vehicle",
+    "Death of vehicle passenger",
+    "Fall over 20 feet",
+    "Roll over of vehicle",
+    "High speed vehicle crash",
+    "Vehicle/Pedestrian crash",
+    "Motorcycle crash",
+    "Unresponsive or altered mental status",
+    "Hidden Injuries",
+    "Other"
+];
--- patient-tracker/src/app/patient.service.ts
+++ patient-tracker/src/app/patient.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import { Patient } from './model/patient';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class PatientService {
 
-  constructor() { }
+  private PATIENTS: Patient[] = [
+    {
+      firstname: 'John',
+      lastname: 'Doe',
+      age: 25
+    }
+  ]
+
+  private patientUrl = '';
+
+  getAllPatients(): Observable<Patient[]> {
+    return of(this.PATIENTS);
+  }
+
+  updatePatient(id: string, patient: Patient): Patient {
+    return patient;
+  }
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 }
