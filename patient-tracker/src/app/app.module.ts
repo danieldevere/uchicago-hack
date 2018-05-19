ff3dc9019890607b30c7e7bf18c4bcfd60823f1d
Dan DeVere <danieldevere285@gmail.com>
2018-05-19 09:57:09 -0500

    fixed angular version

--- patient-tracker/src/app/app.module.ts
+++ patient-tracker/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
