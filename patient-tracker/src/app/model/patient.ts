7ddc408247470c8bd7ae1fbfe153039cbf3a1f37
Dan DeVere <danieldevere285@gmail.com>
2018-05-19 10:19:18 -0500

    setup routes and added components/service

---
+++ patient-tracker/src/app/model/patient.ts
@@ -0,0 +1,8 @@
+export module ProjectAPI  {
+    export interface Patient {
+        id?: string;
+        firstname: string;
+        lastname: string;
+        age: number;
+    }
+}
---
+++ patient-tracker/src/app/routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { EmsComponent } from './ems/ems.component';
+import { WitnessComponent } from './witness/witness.component';
+import { DoctorComponent } from './doctor/doctor.component';
+
+export const AppRoutes: Routes = [
+    { path: '', redirectTo: 'ems', pathMatch:'full' },
+    { path: 'ems', component: EmsComponent },
+    { path: 'witness', component: WitnessComponent },
+    { path: 'doctor', component: DoctorComponent }
+]
