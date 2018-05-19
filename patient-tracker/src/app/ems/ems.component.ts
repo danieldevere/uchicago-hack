46f9bc8e4820d7dc57c4c246676371f1ee8e8a41
Xu <U755966@DOW.COM>
2018-05-19 12:34:51 -0500

    starting

--- patient-tracker/src/app/ems/ems.component.ts
+++ patient-tracker/src/app/ems/ems.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ems',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmsComponent implements OnInit {
 
-  constructor() { }
+  private emsForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
   }
